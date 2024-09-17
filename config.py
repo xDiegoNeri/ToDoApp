f07b96192ba9f2cfd8f2faaff7b0f34befcea753
xDiegoNeri <114936178+xDiegoNeri@users.noreply.github.com>
2024-09-17 12:59:19 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secure-default-key')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'cocapiñaloco')
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL', f'sqlite:///{os.path.join(BASE_DIR, "data", "app.db")}'
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'sqlite:///{os.path.join(BASE_DIR, "data", "app.db")}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static/uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
